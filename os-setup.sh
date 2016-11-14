5ec9566f8125ca514c186a9c0f13c5639a4bd682
seantdg <sm.davis@gmx.com>
2016-11-14 21:38:43 +0000

    Update os-setup.sh

--- os-setup.sh
+++ os-setup.sh
@@ -1,17 +1,1 @@
-# clone this repo
-git clone https://github.com/sean-ff1.git && \
-
-#update the os
-apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && \
-
-#add a lightweight UI
-apt-get install gdm xorg xterm icewm -y && apt-get clean -y && \
-
-#setup docker
-(curl -sSl https://get.docker.com/ | sh) && usermod -aG docker bmo && \
-
-#add my aliases
-echo "source ~/development-environment/aliases.txt" >> ~/.bashrc && \
-
-#reboot
-reboot
+git clone https://github.com/sean-ff1.git && apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install gdm xorg xterm icewm -y && apt-get clean -y && (curl -sSl https://get.docker.com/ | sh) && usermod -aG docker bmo && echo "source ~/development-environment/aliases.txt" >> ~/.bashrc && reboot
