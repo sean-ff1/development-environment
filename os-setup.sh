c6c2b3c886c2764e7742e3be6cf9ee7290ed4495
root <root@landOfOoo.Home>
2016-11-17 17:36:17 +0000

    made os-setup pretty and switch bashrc references to to zshrc

--- os-setup.sh
+++ os-setup.sh
@@ -1,1 +1,23 @@
-git clone https://github.com/sean-ff1/development-environment.git && apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install gdm xorg xfce4-terminal icewm python3-dev python3-pip firefox zsh -y && apt-get clean -y && (curl -sSl https://get.docker.com/ | sh) && usermod -aG docker bmo && echo "source ~/development-environment/aliases.txt" >> ~/.bashrc && pip3 install thefuck && echo "eval '$(thefuck --alias ffs)'" >> ~/.bashrc &&chsh -s $(which zsh) && reboot
+# clone repo
+git clone https://github.com/sean-ff1/development-environment.git && \
+# update os
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && \
+# install UI, firefox, zsh, and other dependencies
+apt-get install gdm xorg xfce4-terminal icewm python3-dev python3-pip firefox zsh -y && \
+
+# clean up
+apt-get clean -y && \
+
+# install docker
+(curl -sSl https://get.docker.com/ | sh) && usermod -aG docker bmo && \
+
+# set up aliases
+echo "source ~/development-environment/aliases.txt" >> ~/.zshrc && \
+# install really useful tools
+pip3 install thefuck && echo "eval $(thefuck --alias ffs)" >> ~/.zshrc && \
+
+#set defaul shell
+chsh -s $(which zsh) && \
+
+#reboot
+reboot
