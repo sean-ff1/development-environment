24d1e0e66ca4bc53347b8038b60634edf43d007b
root <root@landOfOoo.Home>
2016-11-17 22:16:48 +0000

    removed zsh

--- os-setup.sh
+++ os-setup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/sean-ff1/development-environment.git && \
 # update os
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && \
 # install UI, firefox, zsh, and other dependencies
-apt-get install gdm xorg xfce4-terminal icewm python3-dev python3-pip firefox zsh -y && \
+apt-get install gdm xorg xfce4-terminal icewm python3-dev python3-pip firefox acpi -y && \
 
 # clean up
 apt-get clean -y && \
@@ -11,16 +11,10 @@ apt-get clean -y && \
 # install docker
 (curl -sSl https://get.docker.com/ | sh) && usermod -aG docker bmo && \
 
-# zshrc
-touch ~/.zshrc
-
 # set up aliases
-echo "source ~/development-environment/aliases.txt" >> ~/.zshrc && \
+echo "source ~/development-environment/aliases.txt" >> ~/.bashrc && \
 # install really useful tools
-pip3 install thefuck && echo 'eval $(thefuck --alias ffs)' >> ~/.zshrc && \
-
-#set defaul shell
-chsh -s $(which zsh) && \
+pip3 install thefuck && echo 'eval $(thefuck --alias ffs)' >> ~/.bashrc && \
 
 #reboot
 reboot
